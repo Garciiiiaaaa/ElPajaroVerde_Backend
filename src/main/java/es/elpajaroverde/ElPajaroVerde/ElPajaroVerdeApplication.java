17d04d97e115cc47cad9f4f5fb9a1d40481b6406
Garciiiiaaaa <jorgee.ggarciia@gmail.com>
2026-09-10 10:19:03 +0200

    Initial commit

---
+++ src/main/java/es/elpajaroverde/ElPajaroVerde/ElPajaroVerdeApplication.java
@@ -0,0 +1,13 @@
+package es.elpajaroverde.ElPajaroVerde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElPajaroVerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElPajaroVerdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/elpajaroverde/ElPajaroVerde/ElPajaroVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.elpajaroverde.ElPajaroVerde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElPajaroVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
